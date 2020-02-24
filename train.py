00b13466e239be6bbadf12fca0add7ccf37bffdd
poopoo <poopoo1993>
2020-02-24 16:18:39 +0800

    modify import for renaming DQN to LSTM in DQN.py.

--- train.py
+++ train.py
@@ -1,12 +1,17 @@
 from func.readTxtMap import read_map
 from func.draw import show
+from classes.DQN import LSTM
+from classes.robot import Robot
 import torch
 
 
-map = read_map('map/map')
+# initialize variable
+mat_map = read_map('map/map')
 show(map)
+mobile_bot = Robot()
 
 # create DQN
+
 
 # training
 
